ef2b1fbbdb92b408c54421a27586488f9403de8a
rupamnehete <rupamnehete@gmail.com>
2020-11-07 21:50:26 +0530

    billing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './master/item/item.component';
+import { CustomerComponent } from './master/customer/customer.component';
+import { TaxComponent } from './master/tax/tax.component';
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ItemComponent,
+    CustomerComponent,
+    TaxComponent,
+    MainComponent
+],    
   imports: [
     BrowserModule,
     AppRoutingModule
